e4166a5bc613e0b2afb90c828c50cac753c0a46c
R2B Apps <r2b.apps@gmail.com>
2019-12-16 17:16:49 +0100

    public folder

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
-const app = require('express')();
+const express = require('express');
 
-// App responds on '/' path when GET method.
-app.get('/', async function(req, res) {
+// App instance
+const app = express();
+
+// App sets public folder
+// App responds on '/' path with public folder content
+app.use(express.static('public'));
+
+// App responds on '/helloworld' path when GET method
+app.get('/helloworld', async function(req, res) {
   console.log(`Headers: ${JSON.stringify(req.headers)}`);
   console.log(`Body: ${JSON.stringify(req.body)}`);
   res.send('HelloWorld');
 });
 
-// App is listening on localhost and defined env port or 8080 by default. 
+// App is listening on localhost and defined env port or 8080 by default
 app.listen(port = process.env.PORT || 8080, function () {
   console.log(`Server running on http://localhost:${port}`);
 });
