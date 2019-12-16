26b77dabcb8b667beeff072c4b1d4cd90fbfa9e6
R2B Apps <r2b.apps@gmail.com>
2019-12-16 17:53:59 +0100

    post using POSTMAN

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ app.use(express.static('public'));
 // App parses form inputs automatically on body (x-www-form-urlencoded)
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// App parses body as JSON object
+app.use(bodyParser.json());
+
 // App responds on '/helloworld' path when GET method
 app.get('/helloworld', async function(req, res) {
   console.log(`Headers: ${JSON.stringify(req.headers)}`);
@@ -25,6 +28,16 @@ app.post('/login', async function(req, res) {
   // Check: {"username":"bla bla","password":"bla bla"} on body as JSON object
   console.log(`Body: ${JSON.stringify(req.body)}`);
   res.send(`Welcome user ${req.body.username}`);
+});
+
+// App responds on '/comment' path when POST method
+// Test it with POST comment on POSTMAN collections
+app.post('/comment', async function(req, res) {
+  // Check: "content-type":"application/json" on headers
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  // Check: {"title":"bla bla","comment":"bla bla"} on body as JSON object
+  console.log(`Body: ${JSON.stringify(req.body)}`);
+  res.send(`Comment ${req.body.title} received`);
 });
 
 // App is listening on localhost and defined env port or 8080 by default
