3a964986b3da3eac42ad869fb4eac8deeca71cd7
R2B Apps <r2b.apps@gmail.com>
2019-12-16 17:24:54 +0100

    login form

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // App instance
 const app = express();
@@ -7,11 +8,23 @@ const app = express();
 // App responds on '/' path with public folder content
 app.use(express.static('public'));
 
+// App parses form inputs automatically on body (x-www-form-urlencoded)
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // App responds on '/helloworld' path when GET method
 app.get('/helloworld', async function(req, res) {
   console.log(`Headers: ${JSON.stringify(req.headers)}`);
   console.log(`Body: ${JSON.stringify(req.body)}`);
   res.send('HelloWorld');
+});
+
+// App responds on '/login' path when POST method
+app.post('/login', async function(req, res) {
+  // Check: "content-type":"application/x-www-form-urlencoded" on headers
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  // Check: {"username":"bla bla","password":"bla bla"} on body as JSON object
+  console.log(`Body: ${JSON.stringify(req.body)}`);
+  res.send(`Welcome user ${req.body.username}`);
 });
 
 // App is listening on localhost and defined env port or 8080 by default
