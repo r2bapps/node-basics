06cf5b2a32de44f440e9dffd8c2692c5bd63be04
R2B Apps <r2b.apps@gmail.com>
2019-12-17 16:04:56 +0100

    exercise-01

--- exercises/exercise-01.md
+++ exercises/exercise-01.md
@@ -1,5 +1,10 @@
 # Exercise 01: Basic Blog API
 
+Read the following articles:
+
+**Status codes**: https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html
+**REST methods**: https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
+
 ## Entities
 * User
 * Post
@@ -10,6 +15,8 @@
 * name: string
 * surname: string
 * surname2: string
+* creationDate: Date
+* lastModificationDate: Date
 
 ### Post
 * id: string
@@ -19,12 +26,12 @@
 * tags: [string]
 * creationDate: Date
 * lastModificationDate: Date
-* user: User
+* userId: string
 * comments: [Comment]
 
 ### Comment
 * id: string
 * text: string
 * creationDate: Date
-* user: User
-* comments: [Comment]
+* lastModificationDate: Date
+* userId: string
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const inMemoryPosts = [];
+const inMemoryUsers = [];
+
 // App instance
 const app = express();
 
@@ -14,42 +17,154 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // App parses body as JSON object
 app.use(bodyParser.json());
 
-// App responds on '/helloworld' path when GET method
-app.get('/helloworld', async function(req, res) {
-  console.log(`Headers: ${JSON.stringify(req.headers)}`);
-  console.log(`Body: ${JSON.stringify(req.body)}`);
-  res.send('HelloWorld');
+app.get('/users', async function(req, res) {
+  res.send(inMemoryUsers);
+});
+app.delete('/users', async function(req, res) {
+  inMemoryUsers = [];
+  inMemoryPosts = [];
+  res.send(inMemoryUsers);
+});
+app.get('/users/:userId', async function(req, res) {
+  const user = inMemoryUsers.find(user => user.id === req.params.userId);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.delete('/users/:userId', async function(req, res) {
+  const user = inMemoryUsers.find(user => user.id === req.params.userId);
+  if (user) {
+    inMemoryUsers = inMemoryUsers.filter(it => it.id === userId);
+    inMemoryPosts = inMemoryPosts.filter(post => post.userId === user.id);
+    inMemoryPosts.forEach(post => {
+      post.comments = post.comments.filter(comment => comment.userId === user.id);
+    });
+    res.send(user);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.post('/users', async function(req, res) {
+  // req.body: { name, surname, surname2 }
+  const date = new Date();
+  const user = Object.assign({
+    id: (inMemoryUsers.length + 1).toString(10), 
+    creationDate: date,
+    lastModificationDate: date,
+  }, req.body);
+  inMemoryUsers.push(user);
+  res.send(user);
 });
 
-// App responds on '/login' path when POST method
-app.post('/login', async function(req, res) {
-  // Check: "content-type":"application/x-www-form-urlencoded" on headers
-  console.log(`Headers: ${JSON.stringify(req.headers)}`);
-  // Check: {"username":"bla bla","password":"bla bla"} on body as JSON object
-  console.log(`Body: ${JSON.stringify(req.body)}`);
-  res.send(`Welcome user ${req.body.username}`);
+app.get('/posts', async function(req, res) {
+  res.send(inMemoryPosts);
+});
+app.delete('/posts', async function(req, res) {
+  inMemoryPosts = [];
+  res.send(inMemoryPosts);
+});
+app.get('/posts/:postId', async function(req, res) {
+  const posts = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (posts) {
+    res.send(posts);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.delete('/posts/:postId', async function(req, res) {
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    inMemoryPosts = inMemoryPosts.filter(it => it.id === post.id);
+    res.send(post);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.post('/posts', async function(req, res) {
+  // req.body: { title, subitle, text, tags, userId }
+  const date = new Date();
+  const post = Object.assign({
+    id: (inMemoryPosts.length + 1).toString(10), 
+    creationDate: date, 
+    lastModificationDate: date, 
+    comments: [] 
+  }, req.body);
+  inMemoryPosts.push(post);
+  res.send(post);
 });
 
-// App responds on '/comment' path when POST method
-// Test it with POST comment on POSTMAN collections
-app.post('/comment', async function(req, res) {
-  // Check: "content-type":"application/json" on headers
-  console.log(`Headers: ${JSON.stringify(req.headers)}`);
-  // Check: {"title":"bla bla","comment":"bla bla"} on body as JSON object
-  console.log(`Body: ${JSON.stringify(req.body)}`);
-  res.send(`Comment ${req.body.title} received`);
+app.get('/posts/:postId/comments', async function(req, res) {
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    res.send(post.comments);
+  } else {
+    res.status(404);
+    res.end();
+  }
 });
-
-// App responds on '/comment' path when PUT method
-// Test it with POST comment on POSTMAN collections (change to PUT)
-app.put('/comment', async function(req, res) {
-  res.send(`ok`);
+app.delete('/posts/:postId/comments', async function(req, res) {
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    post.comments = [];
+    res.send(post.comments);
+  } else {
+    res.status(404);
+    res.end();
+  }
 });
-
-// App responds on '/comment' path when DELETE method
-// Test it with POST comment on POSTMAN collections (change to DELETE)
-app.delete('/comment', async function(req, res) {
-  res.send(`ok`);
+app.get('/posts/:postId/comments/:commentId', async function(req, res) {
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    const comment = post.comments.find(comment => comment.id === req.params.commentId);
+    if (comment) {
+      res.send(comment);
+    } else {
+      res.status(404);
+      res.end();
+    }
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.delete('/posts/:postId/comments/:commentId', async function(req, res) {
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    const comment = post.comments.find(comment => comment.id === req.params.commentId);
+    if (comment) {
+      post.comments = post.comments.filter(it => it.id === comment.Id);
+      res.send(comment);
+    } else {
+      res.status(404);
+      res.end();
+    }
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+app.put('/posts/:postId/comments', async function(req, res) {
+  // req.body: { text }
+  const post = inMemoryPosts.find(post => post.id === req.params.postId);
+  if (post) {
+    const date = new Date();
+    const comment = Object.assign({
+      id: (post.comments.length + 1).toString(10), 
+      creationDate: date, 
+      lastModificationDate: date
+    }, req.body);
+    post.comments.push(comment);
+    res.send(post);
+  } else {
+    res.status(404);
+    res.end();
+  }
 });
 
 // App is listening on localhost and defined env port or 8080 by default
